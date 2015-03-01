6d430ca1b93a29c55ce849dfb7a2164e73ac3a47
Ledragon <hugues.stefanski@gmail.com>
2015-03-01 22:19:01 +0100

    Binding to router complete Abstract states removed

--- src/bootstrap-ui-navbar.js
+++ src/bootstrap-ui-navbar.js
@@ -8,12 +8,14 @@
                 templateUrl: '../src/bootstrap-ui-navbar.html',
                 scope: {},
                 link: function (scope, elem, attr) {
-                    scope.states = [
-                        {
-                            name: 'state1',
-                            url: '/app/state'
+                    scope.states = [];
+                    var states = $state.get();
+                    states.forEach(function (s, i) {
+                        if (!s.abstract) {
+                            scope.states.push(s);
                         }
-                    ];
+                    });
+
                 }
             };
         }]);
